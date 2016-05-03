878a61499b84a87b478211f3e9d36ffa3be6f17d
DOLEGA Patrycjusz <pdolega15@STUD.IPL.DOM>
2016-05-03 12:05:06 +0200

    break down en fonctions de client.c en ajout client.h

--- client.c
+++ client.c
@@ -9,46 +9,45 @@
 #include    <string.h>
 #include    <stdlib.h>
 #include 	<signal.h>
+
+#include    <netinet/in.h>
+
 #include    <sys/socket.h>
 #include    <sys/types.h>
 
-#include    <netinet/in.h>
 #define PORT 8765
 #define SYS(call) (((call)==-1)? perror(#call), exit(1) : 0)
-#define BUFFER_SIZE 256
+#define BUFFER_SIZE 512
+
+// Methods declaration
 extern void err_handler(int);
 
 int main(int argc, char** argv){
-	int sck,port,lectureRet,writeRet;
+	int sck, lectureRet, writeRet;
 	char buffer[BUFFER_SIZE];
 	struct sockaddr_in addr;
 	struct hostent *host;
 	signal(SIGPIPE,err_handler);
 	struct message *msg;
 
-	if(argc != 2){
-		fprintf(stderr, "Usage: %s  ip\n",argv[0]);
-		exit(1);
-	}
-	if( (sck = socket(AF_INET,SOCK_STREAM,0)) < 0){
-		perror("Client - Probleme de socket");
-		exit(1);
-	}
+	check_args(&argc, argv);
+	
+	get_socket(&sck);
 
+	//TODO: cette fonction foire
+	//get_host(host, argv[1]);
 	host = gethostbyname(argv[1]);
 	if(host == NULL){
 		fprintf(stderr, "Host inconnu");
 		exit(1);
 	}
 
-	if((msg = (struct message*)malloc(sizeof(struct message))) == NULL) {
-		perror("Erreur lors de l'allocation de memoire pour un message...\n");
-		exit(2);
-	}
+	alloc_msg(msg);
 
-	bzero((char*)&addr, sizeof(struct sockaddr_in));
+	//bcopy et bzero sont dépréciées selon le MAN
+	memset((char*)&addr, 0, sizeof(struct sockaddr_in));
 	addr.sin_family = AF_INET;
-	bcopy(host->h_addr,(char*)&addr.sin_addr.s_addr,host->h_length);
+	memcpy((char*) &addr.sin_addr.s_addr, host->h_addr, host->h_length);
     addr.sin_port = htons(PORT);
 
     if(connect(sck, (struct sockaddr *)&addr,sizeof(addr)) < 0){
@@ -81,4 +80,34 @@ int main(int argc, char** argv){
 void err_handler(int unused){
 	fprintf(stderr, "Connection perdue ! \n");
 	return;
+}
+
+void check_args(int *argc, char** argv){
+	if(*argc != 2){
+		fprintf(stderr, "Usage: %s  ip\n",argv[0]);
+		exit(1);
+	}
+}
+
+void get_socket(int *sck){
+	if( (*sck = socket(AF_INET,SOCK_STREAM,0)) < 0){
+		perror("Client - Probleme de socket");
+		exit(1);
+	}
+}
+
+void get_host(struct hostent* ht, char* url){
+	printf("URL : %s \n",*url);
+	ht = gethostbyname(url);
+	if(ht == NULL){
+		fprintf(stderr, "Host inconnu");
+		exit(1);
+	}
+}
+
+void alloc_msg(struct message* msg){
+	if((msg = (struct message*) malloc(sizeof(struct message))) == NULL) {
+		perror("Erreur lors de l'allocation de memoire pour un message...\n");
+		exit(2);
+	}
 }
